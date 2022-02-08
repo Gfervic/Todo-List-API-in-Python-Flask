a5a8638947e79b0e3b1ea9ac48a06882b8a37ee2
@betagon <gon.fervic@gmail.com>
2022-02-08 16:22:02 +0000

    implemented the GET, POST and DELETE methods of the Todos Api. Ready to Deliver

--- src/app.py
+++ src/app.py
@@ -1,13 +1,40 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 app = Flask(__name__)
 
-@app.route('/todos', methods=['GET'])
+
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
+
+@app.route('/todos', methods=['GET'])
 def hello_world():
-    return '<h1>Hello!</h1>'
+    print(todos)
+    json_text = jsonify(todos)
+    return json_text
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.get_json(force=True) # get the request body content in a real python data structure
+    if request_body is None:
+        return "The request body is null", 400
+    if request_body["label"] is None:
+        return "You have to send the label", 400
+    if request_body["done"] is None:
+        return "You have to send the done", 400
+    
+    # print("Incoming request with the following body", request_body)
+    todos.append(request_body)
+    print(todos)
+    return jsonify(todos), 200
+    # return 'Response for the POST todo'
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    todos.remove(todos[position])
+    print(todos)
+    return jsonify(todos), 200
 
 
 if __name__ == '__main__':
